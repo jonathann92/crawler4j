0ae1161918cd5648dbfa020e7764f12493afbf89
Jonathan Nguyen <Jonathann92@gmail.com>
2016-01-28 11:17:43 -0800

    removed more logger

--- src/main/java/edu/uci/ics/crawler4j/submission/crawler/CrawlerController.java
+++ src/main/java/edu/uci/ics/crawler4j/submission/crawler/CrawlerController.java
@@ -1,7 +1,7 @@
 package edu.uci.ics.crawler4j.submission.crawler;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import java.util.Set;
 import java.util.*;
 
@@ -29,9 +29,9 @@ public class CrawlerController {
 	 public static void main(String[] args) throws Exception {
 
 		    if (args.length != 2) {
-		      logger.info("Needed parameters: ");
-		      logger.info("\t rootFolder (it will contain intermediate crawl data)");
-		      logger.info("\t numberOfCralwers (number of concurrent threads)");
+		      //logger.info("Needed parameters: ");
+		      //logger.info("\t rootFolder (it will contain intermediate crawl data)");
+		      //logger.info("\t numberOfCralwers (number of concurrent threads)");
 		      return;
 		    }
 
